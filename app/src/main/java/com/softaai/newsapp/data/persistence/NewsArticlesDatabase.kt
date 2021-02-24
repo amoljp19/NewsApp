336e522e28677daaa257a7c1d37421262e210228
amol pawar <amoljp19@gmail.com>
2021-02-25 04:31:48 +0530

    added room type converter and resolved test time issues now test passed

--- app/src/main/java/com/softaai/newsapp/data/persistence/NewsArticlesDatabase.kt
+++ app/src/main/java/com/softaai/newsapp/data/persistence/NewsArticlesDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.softaai.newsapp.model.Article
 
 @Database(entities = [Article::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class NewsArticlesDatabase : RoomDatabase() {
 
     abstract fun getArticlesDao(): ArticlesDao
