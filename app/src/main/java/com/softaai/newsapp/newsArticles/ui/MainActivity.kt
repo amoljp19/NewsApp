128da8e5b4372ca3abc22205a880fd68e57466c5
amol pawar <amoljp19@gmail.com>
2021-02-24 14:33:08 +0530

    added application class

--- app/src/main/java/com/softaai/newsapp/newsArticles/ui/MainActivity.kt
+++ app/src/main/java/com/softaai/newsapp/newsArticles/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.softaai.newsapp
+package com.softaai.newsapp.newsArticles.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.softaai.newsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
