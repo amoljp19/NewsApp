b78da1d4374cc7bad38886605419e8ca33805fdb
amol pawar <amoljp19@gmail.com>
2021-02-25 02:24:18 +0530

    updated Article as room entity

--- app/src/main/java/com/softaai/newsapp/model/Article.kt
+++ app/src/main/java/com/softaai/newsapp/model/Article.kt
@@ -1,11 +1,17 @@
 package com.softaai.newsapp.model
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.softaai.newsapp.model.Article.Companion.TABLE_NAME
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+@Entity(tableName = TABLE_NAME)
 @JsonClass(generateAdapter = true)
 data class Article(
+    @PrimaryKey
+    var id: Int? = 0,
     @Json(name = "author")
     val author: String,
     @Json(name = "content")
@@ -22,4 +28,8 @@ data class Article(
     val url: String,
     @Json(name = "urlToImage")
     val urlToImage: String
-)
+){
+    companion object {
+        const val TABLE_NAME = "news_articles"
+    }
+}
