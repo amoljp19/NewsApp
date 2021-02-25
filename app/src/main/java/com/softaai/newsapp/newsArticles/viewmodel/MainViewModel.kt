1c0c6517e6a3e55f69b7a28ec9a7446d54d9434b
amol pawar <amoljp19@gmail.com>
2021-02-26 01:33:25 +0530

    added androidx activity and lifecycle dependencies and also created MainViewModel.kt

---
+++ app/src/main/java/com/softaai/newsapp/newsArticles/viewmodel/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.softaai.newsapp.newsArticles.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softaai.newsapp.data.network.State
+import com.softaai.newsapp.data.repository.ArticleRepository
+import com.softaai.newsapp.model.Article
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private val articleRepository: ArticleRepository) :
+    ViewModel() {
+
+    private val _articlesLiveData = MutableLiveData<State<List<Article>>>()
+
+    val articlesLiveData: LiveData<State<List<Article>>> = _articlesLiveData
+
+    fun getArticles() {
+        viewModelScope.launch {
+            articleRepository.getAllArticles()
+                .onStart { _articlesLiveData.value = State.loading() }
+                .map { resource -> State.fromResource(resource) }
+                .collect { state -> _articlesLiveData.value = state }
+        }
+    }
+}
+
+
