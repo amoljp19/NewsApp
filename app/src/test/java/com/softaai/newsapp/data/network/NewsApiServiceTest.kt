c0fe58d3a9845599978dc756b6374cdabcf445b0
amol pawar <amoljp19@gmail.com>
2021-02-26 00:47:45 +0530

    tested retrofit api response using mockWebServer also resolved faced issues like inputstream null and json file not as per response

---
+++ app/src/test/java/com/softaai/newsapp/data/network/NewsApiServiceTest.kt
@@ -0,0 +1,76 @@
+package com.softaai.newsapp.data.network
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@RunWith(JUnit4::class)
+class NewsApiServiceTest {
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var service: NewsApiService
+
+    private lateinit var mockWebServer: MockWebServer
+
+    @Before
+    fun createService() {
+        mockWebServer = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(
+                MoshiConverterFactory.create(
+                    Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+                )
+            )
+            .build()
+            .create(NewsApiService::class.java)
+    }
+
+    @After
+    fun stopService() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun getArticlesTest() = runBlocking {
+        enqueueResponse("/articles.json")
+        val articles = service.getArticles().body()
+
+        assertThat(articles, notNullValue())
+        assertThat(articles!!.size, `is`(2))
+        assertThat(articles[0].title, `is`("title 1"))
+    }
+
+    private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap()) {
+        val inputStream = javaClass.classLoader!!.getResourceAsStream("api-response/$fileName")
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+        for ((key, value) in headers) {
+            mockResponse.addHeader(key, value)
+        }
+
+        mockWebServer.enqueue(
+            mockResponse
+                .setBody(source.readString(Charsets.UTF_8))
+        )
+
+    }
+}
