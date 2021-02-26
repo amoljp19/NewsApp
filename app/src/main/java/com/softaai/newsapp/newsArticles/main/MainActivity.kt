d54582712c2c823bbe47e56a2f2079cbb984e788
amol pawar <amoljp19@gmail.com>
2021-02-26 17:45:11 +0530

    adapter set output shown on screen

--- app/src/main/java/com/softaai/newsapp/newsArticles/main/MainActivity.kt
+++ app/src/main/java/com/softaai/newsapp/newsArticles/main/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.softaai.newsapp.newsArticles.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.viewModels
-import com.softaai.newsapp.R
 import com.softaai.newsapp.data.network.State
 import com.softaai.newsapp.databinding.ActivityMainBinding
+import com.softaai.newsapp.model.Article
 import com.softaai.newsapp.newsArticles.base.BaseActivity
+import com.softaai.newsapp.newsArticles.main.adapter.ArticleListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,18 +16,31 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     override val mViewModel: MainViewModel by viewModels()
 
+    private val mAdapter = ArticleListAdapter(this::onItemClicked)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mViewBinding.root)
 
-
-
+        initView()
     }
 
     override fun onStart() {
         super.onStart()
         getArticles()
         observeArticles()
+    }
+
+    fun initView() {
+        mViewBinding.run {
+            articleRecyclerView.adapter = mAdapter
+        }
+    }
+
+
+    private fun onItemClicked(article: Article, imageView: ImageView) {
+        TODO("Not yet implemented")
     }
 
     private fun getArticles() = mViewModel.getArticles()
@@ -38,6 +52,7 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
                 is State.Success -> {
                     if (state.data.isNotEmpty()) {
                         Toast.makeText(applicationContext, " " + state.data, Toast.LENGTH_SHORT).show()
+                        mAdapter.submitList(state.data.toMutableList())
                     }
                 }
                 is State.Error -> {
