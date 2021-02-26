9af02e6a65ac39b944d72d8718ec29825ede4f01
amol pawar <amoljp19@gmail.com>
2021-02-26 16:44:06 +0530

    added coil dependency, drawable images and ArticleViewHolder class

---
+++ app/src/main/java/com/softaai/newsapp/newsArticles/main/viewholder/ArticleViewHolder.kt
@@ -0,0 +1,24 @@
+package com.softaai.newsapp.newsArticles.main.viewholder
+
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.softaai.newsapp.R
+import com.softaai.newsapp.databinding.ItemArticleBinding
+import com.softaai.newsapp.model.Article
+
+class ArticleViewHolder(private val binding: ItemArticleBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(article: Article, onItemClicked: (Article, ImageView) -> Unit) {
+        binding.articleDescription.text = article.title
+        binding.articleAuthor.text = article.author
+        binding.imageView.load(article.urlToImage) {
+            placeholder(R.drawable.ic_placeholder)
+            error(R.drawable.ic_broken_image)
+        }
+
+        binding.root.setOnClickListener {
+            onItemClicked(article, binding.imageView)
+        }
+    }
+}
